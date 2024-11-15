afde36c7de43772292d739bbef79cd8285bdfd41
Evan <efdelanty@gmail.com>
2024-11-14 16:10:55 -0800

    Added variables to get the data from the user.

--- app/src/main/java/com/zybooks/workingwithdata/ImageryActivity.kt
+++ app/src/main/java/com/zybooks/workingwithdata/ImageryActivity.kt
@@ -1,20 +1,42 @@
 package com.zybooks.workingwithdata
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.zybooks.workingwithdata.NasaAPI.ImageData
 
 class ImageryActivity : AppCompatActivity() {
+    lateinit var longitude: EditText
+    lateinit var latitude: EditText
+    lateinit var dimension: EditText
+    lateinit var date: EditText
+    lateinit var cloudScore: EditText
+    lateinit var recyclerView: RecyclerView
+    lateinit var imageDataSet: ArrayList<ImageData>
+    lateinit var imageCustomAdapter: ImageCustomAdapter
+    lateinit var countEditText: EditText
+    lateinit var getImageButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_imagery)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        //Setting up to handle input
+        val imageLongitude = findViewById<EditText>(R.id.longitudeEditText)
+        val imageLatitude = findViewById<EditText>(R.id.latitudeEditText)
+        val imageDimension = findViewById<EditText>(R.id.dimensionEditText)
+        val imageDate = findViewById<EditText>(R.id.dateEditText)
+        val imageCloudScore = findViewById<EditText>(R.id.cloudScoreEditText)
+        val imageButton = findViewById<Button>(R.id.getImageButton)
+
+        imageButton.setOnClickListener {
+            //TODO do stuff
         }
 
 
